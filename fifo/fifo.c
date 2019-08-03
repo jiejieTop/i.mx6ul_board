8a9331fea32d2d6289a12380d3f4ce052fd52f36
jiejietop <1161959934@qq.com>
2019-08-03 17:02:39 +0800

    update fifo.c

--- fifo/fifo.c
+++ fifo/fifo.c
@@ -51,8 +51,102 @@
 
 #define MAX_BUFFER_SIZE PIPE_BUF /* 4096 定义在于 limits.h 中*/
 
+
+void fifo_read(void)
+{
+    char buff[MAX_BUFFER_SIZE];
+    int fd;
+    int nread;
+
+    printf("***************** read fifo ************************\n");
+    /* 判断有名管道是否已存在，若尚未创建，则以相应的权限创建*/
+    if (access(MYFIFO, F_OK) == -1)
+    {
+        if ((mkfifo(MYFIFO, 0666) < 0) && (errno != EEXIST))
+        {
+            printf("Cannot create fifo file\n");
+            exit(1);
+        }
+    }
+
+    /* 以只读阻塞方式打开有名管道 */
+    fd = open(MYFIFO, O_RDONLY);
+    if (fd == -1)
+    {
+        printf("Open fifo file error\n");
+        exit(1);
+    }
+
+    memset(buff, 0, sizeof(buff));
+
+    if ((nread = read(fd, buff, MAX_BUFFER_SIZE)) > 0)
+    {
+        printf("Read '%s' from FIFO\n", buff);
+    }
+
+   printf("***************** close fifo ************************\n");
+
+    close(fd);
+
+    exit(0);
+}
+
+
+void write_fifo(void)
+{
+    int fd;
+    char buff[] = "this is a fifo test demo";
+    int nwrite;
+
+    sleep(2);   //等待子进程先运行
+
+    /* 以只写阻塞方式打开 FIFO 管道 */
+    fd = open(MYFIFO, O_WRONLY | O_CREAT, 0644);
+    if (fd == -1)
+    {
+        printf("Open fifo file error\n");
+        exit(1);
+    }
+
+    /*向管道中写入字符串*/
+    if ((nwrite = write(fd, buff, MAX_BUFFER_SIZE)) > 0)
+    {
+        printf("Write '%s' to FIFO\n", buff);
+    }
+    
+    if(wait(NULL))  //等待子进程退出
+    {
+        close(fd);
+        exit(0);
+    }
+
+}
+
+
 int main()
 {
+    pid_t result;
+    /*调用 fork()函数*/
+    result = fork();
+
+    /*通过 result 的值来判断 fork()函数的返回情况，首先进行出错处理*/
+    if(result == -1)
+    {
+        printf("Fork error\n");
+    }
+
+
+    else if (result == 0) /*返回值为 0 代表子进程*/
+    {
+        fifo_read();
+    }
+
+    else /*返回值大于 0 代表父进程*/
+    {
+        write_fifo();
+    }
+
+    return result;
 }
 
 
